61f26d39f8b215f4a6bfe060ac2b0deaf4529230
Santiago Spina <santiagospina025@gmail.com>
2025-07-08 12:55:39 +0200

    Funcionalidad de celdas agregada
    cuando el usuario no adivina la camiseta, se revela una casilla random

--- lib/application/pages/game/game_page.dart
+++ lib/application/pages/game/game_page.dart
@@ -35,6 +35,8 @@ class _GamePageState extends State<GamePage> {
 
   List<String> attempts = [];
 
+  List<bool> revealedCells = List.generate(9, (_) => false);
+
   @override
   void initState() {
     super.initState();
@@ -59,6 +61,83 @@ class _GamePageState extends State<GamePage> {
         filteredTeams = [];
       }
     });
+  }
+
+  void checkAnswer() {
+    final guess = _guessController.text.trim();
+
+    if (guess.isEmpty || attempts.length >= 6) return;
+
+    setState(() {
+      attempts.add(guess);
+    });
+
+    String jerseyTeam = widget.jerseyChallenge.teamName;
+    int jerseyYear = widget.jerseyChallenge.year;
+    String jerseyRealYear = "$jerseyYear-${jerseyYear + 1}";
+
+    bool isCorrect = guess == jerseyTeam;
+
+    if (isCorrect) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Correct! ✅", textAlign: TextAlign.center),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Congratulations, you guessed the right team",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16),
+              Image.asset(widget.jerseyChallenge.imagePath, height: 150),
+              SizedBox(height: 16),
+              Text("Team: $jerseyTeam\nYear: $jerseyRealYear"),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Ok"),
+            ),
+          ],
+        ),
+      );
+    } else {
+      _revealRandomCell();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Respuesta incorrecta. ¡Intenta de nuevo!"),
+          backgroundColor: Colors.redAccent,
+          duration: Duration(milliseconds: 1500),
+        ),
+      );
+    }
+    _guessController.clear();
+  }
+
+  void _revealRandomCell() {
+    /// Logica para revelar una celda aleatoria
+
+    final unrevealedCells = [];
+
+    //Recorre todas las celdas para ver cuales no han sido reveladas
+    for (int i = 0; i < revealedCells.length; i++) {
+      if (!revealedCells[i]) {
+        unrevealedCells.add(i);
+      }
+    }
+
+    if (unrevealedCells.isNotEmpty) {
+      //Selecciona una celda aleatoria de las no reveladas
+      final randomIndex = (unrevealedCells..shuffle()).first;
+
+      //Actualiza el estado para revelar la celda seleccionada
+      setState(() {
+        revealedCells[randomIndex] = true;
+      });
+    }
   }
 
   @override
@@ -104,7 +183,7 @@ class _GamePageState extends State<GamePage> {
       child: LayoutBuilder(
         builder: (context, constraints) {
           final cellWidth = constraints.maxWidth / 3;
-          final cellHeight = 400 / 2;
+          final cellHeight = 400 / 3;
           final aspectRatio = cellWidth / cellHeight;
 
           return Stack(
@@ -118,15 +197,17 @@ class _GamePageState extends State<GamePage> {
               Positioned.fill(
                 child: GridView.builder(
                   physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 6,
+                  itemCount: 9,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 3,
                     crossAxisSpacing: 0,
                     mainAxisSpacing: 0,
                     childAspectRatio: aspectRatio,
                   ),
-                  itemBuilder: (context, index) =>
-                      Container(color: Colors.blueGrey[900]),
+                  itemBuilder: (context, index) {
+                    if (revealedCells[index]) return const SizedBox.shrink();
+                    return Container(color: Colors.blueGrey[900]);
+                  },
                 ),
               ),
             ],
@@ -189,58 +270,5 @@ class _GamePageState extends State<GamePage> {
         }),
       ),
     );
-  }
-
-  void checkAnswer() {
-    final guess = _guessController.text.trim();
-
-    if (guess.isEmpty || attempts.length >= 6) return;
-
-    setState(() {
-      attempts.add(guess);
-    });
-
-    String jerseyTeam = widget.jerseyChallenge.teamName;
-    int jerseyYear = widget.jerseyChallenge.year;
-    String jerseyRealYear = "$jerseyYear-${jerseyYear + 1}";
-
-    bool isCorrect = guess == jerseyTeam;
-
-    if (isCorrect) {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text("Correct! ✅", textAlign: TextAlign.center),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                "Congratulations, you guessed the right team",
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 16),
-              Image.asset(widget.jerseyChallenge.imagePath, height: 150),
-              SizedBox(height: 16),
-              Text("Team: $jerseyTeam\nYear: $jerseyRealYear"),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text("Ok"),
-            ),
-          ],
-        ),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Respuesta incorrecta. ¡Intenta de nuevo!"),
-          backgroundColor: Colors.redAccent,
-          duration: Duration(milliseconds: 1500),
-        ),
-      );
-    }
-    _guessController.clear();
   }
 }
